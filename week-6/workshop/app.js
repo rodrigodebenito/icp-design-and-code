bc348fc7b031a1dcf9085234ce13c0fbb2e63bc8
Rodrigo de Benito Sanz <rodrigo.debenitosanz@nytimes.com>
2017-05-17 17:39:21 -0400

    Add annotations and extra route

--- week-6/workshop/app.js
+++ week-6/workshop/app.js
@@ -1,10 +1,22 @@
+// Open the workshop folder.
+// Enter `npm install` to install required pakages.
+// Enter `node app.js` on terminal to kick it off.
 var express = require('express');
 var app = express();
 
+// Set default GET route.
+// Test: `curl -X GET http://localhost:3000`
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello GET World!\n');
 });
 
+// Set default POST route.
+// Test: `curl -X POST http://localhost:3000`
+app.post('/', function (req, res) {
+  res.send('Hello POST World!\n');
+});
+
+// Start your server on port 3000
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
